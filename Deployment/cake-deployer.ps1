872be19db3c7182c66a9a31b02a16f0f3da33002
Daniel Robaszkiewicz <daniel.robaszkiewicz@modelingevolution.com>
2023-08-17 13:31:56 +0200

    clean-up

--- Deployment/cake-deployer.ps1
+++
@@ -1,3 +0,0 @@
-New-Cake -Name "Identity.Deployer" -Root "../Source" | Out-Null
-Add-CakeCommonStep Pack { Build-Nuget -SpecFile "octopus-deploy.nuspec" }
-Add-CakeCommonStep Publish { Publish-Nuget -SourceUrl "https://nuget.modelingevolution.com"}
--- Deployment/cake.ps1
+++
@@ -1,3 +0,0 @@
-New-Cake -Name "TcpMultiplexer.Server" -Root "../Source" | Out-Null
-Add-CakeCommonStep Build { Build-DockerImage -ImageName "tcpmultiplexer" -IsLatest -Dockerfile ./TcpMultiplexer.Server/Dockerfile -Repository "docker.modelingevolution.com"  }
-Add-CakeCommonStep Deploy { Publish-DockerImage }
